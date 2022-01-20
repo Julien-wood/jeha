7d50609cc8f12b37e849a5d337b22c69253ab099
Julien-wood <julienwoodbox@gmail.com>
2022-01-20 22:58:21 +0000

    la création de la page d'accueil

---
+++ index.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asso-JEHA || Jeunes Entrepreneurs Hexa-Alliance</title>
+    <meta name="description" content="Association Humanitaire Démunie Entrepreneur voyage Stage Don Aides Aux Enfants Soutien
+    Soutenir Projets ">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- ====== style ======= -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+</head>
+<body>
+  
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><a href="tel:+22890319573">22890319573</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><a href="tel:+22898485549">22898485549</a></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-youtube"></i></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html">ASSO - JEHA</a></h1>
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
+          <li><a class="nav-link scrollto" href="#about">A Propos</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Blog</a></li>
+          <li><a class="nav-link scrollto" href="#team">Projets</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#contact">NOUS SOUTENIR</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- ===== .navbar ===== -->
+
+    </div>
+  </header>
+  <!-- ====== End Header ====== -->
+    <!-- ***** Main Banner Area Start ***** -->
+    <section class="section main-banner" id="top" data-section="section1">
+        <video autoplay muted loop id="bg-video">
+            <source src="assets/video/video.mp4" type="video/mp4" />
+        </video>
+
+        <div class="video-overlay header-text">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12">
+                    <div class="caption">
+                <h5>Bonjour !</h5>
+                <h2>Bienvenue sur le site de l'Asso-JEHA</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat repellendus quibusdam harum, nemo quam consequuntur nam corporis distinctio voluptas fugit ad aut inventore necessitatibus reiciendis exercitationem dolores? Quia, voluptatem ex expedita consectetur rem id veniam quos odit illo eius. Optio officiis ipsum deserunt a fugit voluptate..</p>
+                <div class="main-button-red">
+                    <div class="scroll-to-section btn btn-info"><a class="text-black" href="#contact">Agir avec Nous</a></div>
+                </div>
+            </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <!-- ====== Main Banner Area End ======= -->
+    <div class="container">
+        <!-- ===== debut boxes section ===== -->
+        <div class="row boxes">
+            <div class="col  box">
+                <div class="icon"><img src="assets/imgs/icon/idee.gif" alt=""></i></div>
+                <div class="titre-box">
+                    <h2>Entrepreneuriat</h2>
+                </div>
+                <div class="text-box">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ut debitis corporis aperiam 
+                        animi earum obcaecati, accusantium a ipsa soluta tempore laborum rem ex. Rerum, consequatur 
+                        necessitatibus non perferendis delectus voluptates dolorem asperiores nisi tempora quas, 
+                        cumque, laudantium ab. Porro!</p>
+                </div>
+            </div>
+            <div class="col  box">
+                <div class="icon"><img src="assets/imgs/icon/la-protection-sociale.gif" alt=""></div>
+                <div class="titre-box">
+                    <h2>Projets Humanitaire</h2>
+                </div>
+                <div class="text-box">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ut debitis corporis aperiam 
+                        animi earum obcaecati, accusantium a ipsa soluta tempore laborum rem ex. Rerum, consequatur 
+                        necessitatibus non perferendis delectus voluptates dolorem asperiores nisi tempora quas, 
+                        cumque, laudantium ab. Porro!</p>
+                </div>
+            </div>
+            <div class="col  box">
+                <div class="icon"><img src="assets/imgs/icon/poignee-de-main.gif" alt=""></div>
+                <div class="titre-box">
+                    <h2>Bénévolat</h2>
+                </div>
+                <div class="text-box">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ut debitis corporis aperiam 
+                        animi earum obcaecati, accusantium a ipsa soluta tempore laborum rem ex. Rerum, consequatur 
+                        necessitatibus non perferendis delectus voluptates dolorem asperiores nisi tempora quas, 
+                        cumque, laudantium ab. Porro!</p>
+                </div>
+            </div>
+            <div class="col  box">
+                <div class="icon"><img src="assets/imgs/icon/don.gif" alt=""></div>
+                <div class="titre-box">
+                    <h2>Nous Soutenir</h2>
+                </div>
+                <div class="text-box">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, ut debitis corporis aperiam 
+                        animi earum obcaecati, accusantium a ipsa soluta tempore laborum rem ex. Rerum, consequatur 
+                        necessitatibus non perferendis delectus voluptates dolorem asperiores nisi tempora quas, 
+                        cumque, laudantium ab. Porro!</p>
+                </div>
+            </div>
+        </div>  <!-- ===== fin boxes section ===== -->
+<hr>
+        <!-- ===== derniers articles & Présentation ===== -->
+        <div class="row main">
+            <!-- ====== les derniers artices ====== -->
+            <div class="col-9">
+                <div class="row">
+                    <div class="title">
+                        <h2>Nos Articles Récents</h2>
+                        <p>Retrouvez ici nos articles les plus récents et découvrir nos évènements et activités</p>
+                    </div>
+                    <div class="col-6 article">
+                        <div class="image"><img src="assets/imgs/recycling.png" alt=""></div>
+                        <div class="titre-article"><h3>Premier titre</h3></div>
+                        <div class="description"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias eius, fugiat rerum quisquam vitae nostrum blanditiis et ad, ipsa sapiente numquam at corporis a ducimus ab officiis veniam qui laudantium.</p></div>
+                        <div class="button"><button><a href="">lire complet</a></button></div>
+                    </div>
+                    <div class="col-6 article">
+                        <div class="image"><img src="assets/imgs/eco.png" alt=""></div>
+                        <div class="titre-article"><h3>Second titre</h3></div>
+                        <div class="description"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam impedit delectus tenetur consectetur doloremque, veritatis corporis adipisci eum vitae provident fuga nobis sapiente doloribus, unde pariatur magni aperiam reiciendis officiis!</p></div>
+                        <div class="button"><button><a href="">lire complet</a></button></div>
+                    </div>
+                </div>
+            </div> <!-- ====== Fin des derniers articles ====== -->
+
+            <!-- ====== Présentation du projet mère ====== -->
+            <div class="col-3 cognotte">
+                <div class="img-don"><img src="assets/imgs/eco2.png" alt=""></div>
+                <div class="titre-don"><i class="bi bi-piggy-bank"><h3>Notre Cagnotte</h3></i></div>
+                <div class="desc"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolor iste aperiam fugit exercitationem quae odio reprehenderit sint. Et repellat facilis ipsam aliquid id veniam officia debitis explicabo. Suscipit, quod?</p></div>
+                <div class="soutenir"><button><a href="http://" target="_blank" rel="noopener noreferrer">Nous Soutenir</a></button></div>
+            </div>
+        </div>
+        <hr>
+    </div> <!-- ====== Fin de la Présentation du projet mère ====== -->
+
+    <main class="container main">
+        <div class="row">
+            <div class="col-9">
+                <div class="row">
+                    <div class="title">
+                        <h2>Projets à la une</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, voluptas maiores. Itaque eum dignissimos laudantium.</p>
+                    </div>
+                    <div class="col-4 projet">
+                        <div class="img-pro">
+                            <img src="" alt="">
+                            <div class="price"><h4>450£</h4></div>
+                        </div>
+                        <div class="titr"><h3>Titre Projet 1</h3></div>
+                        <div class="info">
+                            <div class="nom"><h6>Julien WOOD</h6></div>
+                            <div class="type"><h6>Technologie</h6></div>
+                        </div>
+                        <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et repellat tempore iusto, amet tempora, nulla minima ex porro consequatur quo reprehenderit? Voluptatibus harum similique, ullam maxime quo quasi placeat. Voluptatum.</div>
+                        <div class="button"><a href="http://" target="_blank" rel="noopener noreferrer"><h5>Contribuer</h5></a></div>
+                    </div>
+                    <div class="col-4 projet">
+                        <div class="img-pro">
+                            <img src="" alt="">
+                            <div class="price"><h4>450£</h4></div>
+                        </div>
+                        <div class="titr"><h3>Titre Projet 1</h3></div>
+                        <div class="info">
+                            <div class="nom"><h6>Julien WOOD</h6></div>
+                            <div class="type"><h6>Technologie</h6></div>
+                        </div>
+                        <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et repellat tempore iusto, amet tempora, nulla minima ex porro consequatur quo reprehenderit? Voluptatibus harum similique, ullam maxime quo quasi placeat. Voluptatum.</div>
+                        <div class="button"><a href="http://" target="_blank" rel="noopener noreferrer"><h5>Contribuer</h5></a></div>
+                    </div>
+                    <div class="col-4 projet">
+                        <div class="img-pro">
+                            <img src="" alt="">
+                            <div class="price"><h4>450£</h4></div>
+                        </div>
+                        <div class="titr"><h3>Titre Projet 1</h3></div>
+                        <div class="info">
+                            <div class="nom"><h6>Julien WOOD</h6></div>
+                            <div class="type"><h6>Technologie</h6></div>
+                        </div>
+                        <div class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et repellat tempore iusto, amet tempora, nulla minima ex porro consequatur quo reprehenderit? Voluptatibus harum similique, ullam maxime quo quasi placeat. Voluptatum.</div>
+                        <div class="button"><a href="http://" target="_blank" rel="noopener noreferrer"><h5>Contribuer</h5></a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 remercier">
+                <div class="row">
+                    
+                    <div class="col-6">
+                        <span><h5>donateur</h5></span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia vero distinctio!
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <span><h5>donateur</h5></span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia vero distinctio!
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <span><h5>donateur</h5></span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia vero distinctio!
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <span><h5>donateur</h5></span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia vero distinctio!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </main>
+    <section>
+        <div class="container">
+            <div class="row main">
+                <div class="title">
+                    <h2>NOTRE EQUIPE</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, voluptas maiores. Itaque eum dignissimos laudantium.</p>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+                <div class="col teams">
+                    <div class="photo"><img src="" alt=""></div>
+                    <div class="nom"><h4>Julien WOOD</h4></div>
+                    <div class="role">Président</div>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </section>
+    <section>
+        <footer>
+            <div class="container-flude">
+                <div class="container">
+                <div class="row ">
+                    <div class="col left">
+                        <p><h4>Copyright © 2022.  <br> <br> T-LEX., Touts droits réservés.</h4></p>
+                    </div>
+                    <div class="col center">
+                        <p>
+                            ASSO-JEHA || Jeunes Entrepreneurs Hexa-Alliance 
+                            <br>
+                            Enregistré : 
+                            <br>
+                            Récipissé :
+                        </p>
+                    </div>
+                    <div class="col right">
+                        <ul>
+                            <li><i class="bi  bi-twitter"></i></li>
+                            <li><i class="bi bi-facebook"></i></li>
+                            <li><i class="bi bi-instagram"></i></li>
+                            <li><i class="bi bi-youtube"></i></li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+
+        <!-- Vendor JS Files -->
+        <script src="assets/vendor/purecounter/purecounter.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="assets/js/main.js"></script>
+
+</body>
+</html>
+
+
+
